984705bc2288e56b54182abd4835fe7c27480dde
Matthew Nolan <mnolan132@gmail.com>
2025-04-16 17:36:29 +1200

    Refactor Link to use useNavigate()

--- src/pages/components/componentCard.tsx
+++ src/pages/components/componentCard.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Component } from "@/types";
 import { Card, Link } from "@chakra-ui/react";
 import { CiLink } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
 
 export const ComponentCard = ({ item }: { item: Component }) => {
+    const navigate = useNavigate();
+
   return (
     <Card.Root className="cursor-pointer">
       <Card.Body display={"flex"} justifyContent={"space-evenly"}>
         <Card.Title>{item.name}</Card.Title>
         <Card.Description>{item.description}</Card.Description>
         <Card.Footer>
-          <Link href={`/components/${item.id}`} colorPalette={"cyan"}>
+          <Link onClick={() => navigate(`/components/${item.id}`)} colorPalette={"cyan"}>
             {`View documentation`} <CiLink />
           </Link>
         </Card.Footer>
